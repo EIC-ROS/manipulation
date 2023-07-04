39cdf09aed8598d9a2319bb0d8e9771fe4423534
walkie the robot <99135588+walkie-eic@users.noreply.github.com>
2023-07-04 14:30:15 +0200

    hopefully it is an update

--- simple_environment_exporter/src/simple_environment_exporter/__init__.py
+++ simple_environment_exporter/src/simple_environment_exporter/__init__.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import
 
 from . import _import
-from . import (Simple_Environment_eXporter)
+from . import Simple_Environment_eXporter
+from . import SEX_Manipulation
+from . import object_exporter
